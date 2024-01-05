a89fd5f41be4cb22c1205e1c05a388150e047a63
Lucas CHEN(陳奕廷) <lucaschen@gamania.com>
2024-01-05 17:13:16 +0800

    add lint format setting

--- app/models/schemas/chatbot_schema.py
+++ app/models/schemas/chatbot_schema.py
@@ -2,36 +2,21 @@
 from typing import Optional
 from pydantic import BaseModel, Field
 
-class CreateChatbotInput(BaseModel):
+
+class CreateChatbot(BaseModel):
     chatbot_id: Optional[int] = Field(default=0, description="chatbot id，預設為0")
     description: Optional[str] = Field(default="", description="chatbot 的相關說明")
 
     class Config:
         from_attributes = True
-        json_schema_extra = {"example": {"chatbot_id": True, "description": "chatbot test"}}
-
-class CreateChatbotOutput(BaseModel):
-    message: str = Field(..., description="chatbot 的相關說明")
-
-    class Config:
-        from_attributes = True
         json_schema_extra = {
-            "example": [
-                {
-                    "message": "chatbot test"
-                }
-            ]
+            "example": {"chatbot_id": 0, "description": "chatbot test"}
         }
+
 
 class GetAllChatbots(BaseModel):
     message: str = Field(..., description="chatbot 的相關說明")
 
     class Config:
         from_attributes = True
-        json_schema_extra = {
-            "example": [
-                {
-                    "message": "chatbot test"
-                }
-            ]
-        }
+        json_schema_extra = {"example": [{"message": "chatbot test"}]}
